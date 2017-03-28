3335ac66a9a452ec8ffcc4677a63fe272ef782cb
John Tsantilis <i.tsantilis@yahoo.com>
2017-03-28 15:09:11 +0300

    code refactoring

--- src/main/java/org/lumi/dockerjava/DockerJavaDriverApp.java
+++ src/main/java/org/lumi/dockerjava/DockerJavaDriverApp.java
@@ -4,14 +4,18 @@ import com.github.dockerjava.api.DockerClient;
 import com.github.dockerjava.api.command.CreateContainerResponse;
 import com.github.dockerjava.api.command.DockerCmdExecFactory;
 import com.github.dockerjava.api.model.ExposedPort;
+import com.github.dockerjava.api.model.HostConfig;
 import com.github.dockerjava.api.model.Ports;
 import com.github.dockerjava.api.model.SearchItem;
 import com.github.dockerjava.core.DefaultDockerClientConfig;
 import com.github.dockerjava.core.DockerClientBuilder;
 import com.github.dockerjava.core.DockerClientConfig;
 import com.github.dockerjava.jaxrs.JerseyDockerCmdExecFactory;
+import org.apache.commons.io.FileUtils;
 
 import java.util.List;
+
+import static com.github.dockerjava.api.model.Capability.SYS_ADMIN;
 
 /**
  * Created by John Tsantilis
@@ -42,23 +46,23 @@ public class DockerJavaDriverApp {
                 .build();
 
         //Search Docker repository
-        List<SearchItem> dockerSearch = dockerClient.searchImagesCmd("fiware/orion").exec();
+        List<SearchItem> dockerSearch = dockerClient.searchImagesCmd("giannis20012001/phpdashboard").exec();
         System.out.println("Search returned" + dockerSearch.toString());
 
         //
-        ExposedPort tcp = ExposedPort.tcp(1026);
+        ExposedPort tcp = ExposedPort.tcp(80);
         Ports portBindings = new Ports();
         System.out.println(tcp.toString());
-        portBindings.bind(tcp, new Ports.Binding("0.0.0.0", "1026"));
+        portBindings.bind(tcp, new Ports.Binding("0.0.0.0", "80"));
 
         final String testVariable = "dbhost 172.17.0.1";
-        CreateContainerResponse container = dockerClient.createContainerCmd("fiware/orion")
+        HostConfig hostConfig = new HostConfig().withPrivileged(true);
+        CreateContainerResponse container = dockerClient.createContainerCmd("giannis20012001/phpdashboard")
                 .withExposedPorts(tcp)
                 .withPortBindings(portBindings)
-                .withName("orion")
+                .withName("phpdashboard")
                 .withPublishAllPorts(true)
-                //.withEnv(testVariable)
-                .withCmd("-dbhost 172.17.0.1")
+                .withHostConfig(hostConfig)
                 .exec();
 
         dockerClient.startContainerCmd(container.getId()).exec();
