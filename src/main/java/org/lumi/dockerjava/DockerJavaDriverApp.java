d07264bf7bc6cc579e0531534573e4829f418705
John Tsantilis <i.tsantilis@yahoo.com>
2017-06-27 14:49:42 +0300

    code refactoring

--- src/main/java/org/lumi/dockerjava/DockerJavaDriverApp.java
+++ src/main/java/org/lumi/dockerjava/DockerJavaDriverApp.java
@@ -6,16 +6,10 @@ import com.github.dockerjava.api.command.DockerCmdExecFactory;
 import com.github.dockerjava.api.model.ExposedPort;
 import com.github.dockerjava.api.model.HostConfig;
 import com.github.dockerjava.api.model.Ports;
-import com.github.dockerjava.api.model.SearchItem;
 import com.github.dockerjava.core.DefaultDockerClientConfig;
 import com.github.dockerjava.core.DockerClientBuilder;
 import com.github.dockerjava.core.DockerClientConfig;
 import com.github.dockerjava.jaxrs.JerseyDockerCmdExecFactory;
-import org.apache.commons.io.FileUtils;
-
-import java.util.List;
-
-import static com.github.dockerjava.api.model.Capability.SYS_ADMIN;
 
 /**
  * Created by John Tsantilis
@@ -27,11 +21,6 @@ public class DockerJavaDriverApp {
         DockerClientConfig config = DefaultDockerClientConfig.createDefaultConfigBuilder()
                 .withDockerHost("tcp://195.46.17.228:2375")
                 .withDockerTlsVerify(false)
-                //.withDockerCertPath("/home/user/.docker")
-                //.withRegistryUrl("https://index.docker.io/v2/")
-                //.withRegistryUsername("giannis20012001")
-                //.withRegistryPassword("Floor53*")
-                //.withRegistryEmail("i.tsantilis@yahoo.com")
                 .withApiVersion("1.25")
                 .build();
 
@@ -45,17 +34,11 @@ public class DockerJavaDriverApp {
                 .withDockerCmdExecFactory(dockerCmdExecFactory)
                 .build();
 
-        //Search Docker repository
-        List<SearchItem> dockerSearch = dockerClient.searchImagesCmd("giannis20012001/phpdashboard").exec();
-        System.out.println("Search returned" + dockerSearch.toString());
-
         //
         ExposedPort tcp = ExposedPort.tcp(80);
         Ports portBindings = new Ports();
-        System.out.println(tcp.toString());
         portBindings.bind(tcp, new Ports.Binding("0.0.0.0", "80"));
 
-        final String testVariable = "dbhost 172.17.0.1";
         HostConfig hostConfig = new HostConfig().withPrivileged(true);
         CreateContainerResponse container = dockerClient.createContainerCmd("giannis20012001/phpdashboard")
                 .withExposedPorts(tcp)
